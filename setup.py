0e43e220c815289267adfca9b0b1ee31da197b59
Mazin Ahmed <mazin@mazinahmed.net>
2026-02-23 20:39:52 -0800

    wip

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+import shutil
+
 from setuptools import setup
+from setuptools.command.install_scripts import install_scripts
+
+
+class RenameScript(install_scripts):
+    def run(self):
+        install_scripts.run(self)
+        for f in self.get_outputs():
+            if f.endswith("git-sign.py"):
+                dest = f[:-3]  # strip .py
+                shutil.move(f, dest)
+
 
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
@@ -13,6 +26,7 @@ setup(
     author_email="mazin@mazinahmed.net",
     url="https://github.com/mazen160/git-sign",
     scripts=["git-sign.py"],
+    cmdclass={"install_scripts": RenameScript},
     python_requires=">=3.7",
     license="MIT",
     classifiers=[
